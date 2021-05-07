a00efde6ba3ee9f04aa6bf3bebeb828e2d575a9c
KevinHenleyCode <kevinhenleyinfo@gmail.com>
2021-05-07 04:24:21 -0500

    created two different prompts

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const inquirer = require('inquirer');
 // let repeat = true
 const fs = require('fs');
 
-
+let info = []
 const htmlContent = (userInput) =>
 
     ``;
@@ -18,26 +18,11 @@ const htmlContent = (userInput) =>
 
 
 let workers = ['Manager', 'Engineer', 'Intern']
-let list1 = [{
-
-    type: 'list',
-    name: 'employee',
-    message: 'Employee select',
-    choices: workers,
-    function: test = (val) => {
-        console.log(val)
-    }
-},
+let manager = [
 {
     type: 'input',
     name: 'name',
     message: 'Employee name?',
-    filter: (val) => {
-        return val.toLowerCase();
-        if (1 === 1) {
-
-        }
-    },
 },
 {
     type: 'number',
@@ -56,45 +41,197 @@ let list1 = [{
     message: 'Employee email?',
     validate: (val) => {
         if (val == '@') {
-            console.log('hello bob');
             return true;
         }
     }
 },
+{
+    type: 'number',
+    name: 'officNumber',
+    message: 'Office number?',
+    validate: (val) => {
+        if (val === toNumber(val)) {
+            return true;
+        }
+        return 'need a number'
+    }
+},
+{
+    type: 'list',
+    name: 'employee',
+    message: 'Employee select',
+    choices: workers
+}
+]
+let engineer = [
+{
+    type: 'input',
+    name: 'name',
+    message: 'Employee name?',
+},
+{
+    type: 'number',
+    name: 'id',
+    message: 'Employee id?',
+    validate: (val) => {
+        if (val === toNumber(val)) {
+            return true;
+        }
+        return 'need a number'
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Employee email?',
+    validate: (val) => {
+        if (val == '@') {
+            return true;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'GitHub username:'
+},
+{
+    type: 'list',
+    name: 'finished',
+    message: 'Finish building team?',
+    choices: ['yes', 'no']
+},
+{
+        type: 'list',
+        name: 'employee',
+        message: 'Employee select',
+        choices: workers
+}
+]
+let intern = [
+{
+    type: 'input',
+    name: 'name',
+    message: 'Employee name?',
+},
+{
+    type: 'number',
+    name: 'id',
+    message: 'Employee id?',
+    validate: (val) => {
+        if (val === toNumber(val)) {
+            return true;
+        }
+        return 'need a number'
+    }
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Employee email?',
+    validate: (val) => {
+        if (val == '@') {
+            return true;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'school',
+    message: 'What school do you attend?'
+},
+{
+    type: 'list',
+    name: 'finished',
+    message: 'Finish building team?',
+    choices: ['yes', 'no']
+},
+{
+        type: 'list',
+        name: 'employee',
+        message: 'Employee select',
+        choices: workers
+}
 ]
 
 
-let manage = 0
+
+
+let choice = manager
+
+// if (userInput.employee == 'Manager') {
+//     console.log(list2[0]);
+// }
+
 
 function ask() {
 
 
     inquirer.prompt([
-        list1[0],
+
         {
             type: 'list',
-            name: 'repeat',
-            message: 'Finish building team?',
-            choices: ['yes', 'no']
-        },
-    ])
-        .then((userInput) => {
+            name: 'employee',
+            message: 'Employee select',
+            choices: workers
+        }
 
-            const htmlFile = htmlContent(userInput);
 
-            if (userInput.employee == 'Manager') {
-                workers.splice(0, 1)
-                // return manage = 1
+    ]).then((userInput) => {
 
-            }
+        if (userInput.employee == 'Manager') {
 
-            if (userInput.repeat == 'no') {
-                ask()
-            } else {
+            workers.splice(0, 1)
+            ask2(userInput)
+            
+        }
 
-                fs.writeFile('index.html', htmlFile, (error) =>
-                    error ? console.log(error) : console.log(`Your html is ready!${JSON.stringify(userInput)}`));
-            }
-        })
+        const htmlFile = htmlContent(userInput);
+
+        
+    })
 }
+
+
 ask()
+
+const ask2 = (userInput) => {
+
+    inquirer.prompt([
+
+        ...choice
+
+    ]).then((userInput) => {
+
+        if (userInput.employee == 'Engineer') {
+            choice = engineer
+
+        }
+        if (userInput.employee == 'Intern') {
+            choice = intern
+
+        }
+
+        const htmlFile = htmlContent(userInput);
+        let manage = 0
+        if (manage == 0) {
+
+            workers.splice(0, 1)
+            ask2()
+            return manage = 1
+
+        }
+
+        if (userInput.finished == 'no') {
+
+            ask2()
+        } else {
+
+            fs.writeFile('index.html', htmlFile, (error) =>
+                error ? console.log(error) : console.log(`Your html is ready!${JSON.stringify(userInput)}`));
+        }
+
+    })
+
+}
+
